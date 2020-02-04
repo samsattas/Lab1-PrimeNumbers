a22d3310f3c2a9c5b5382279cc9ea5af216311e9
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-02-04 12:44:02 -0500

    Alpha v1.1

--- src/model/PrimeNumbers.java
+++ src/model/PrimeNumbers.java
@@ -2,4 +2,36 @@ package model;
 
 public class PrimeNumbers {
 
+	public PrimeNumbers() {
+		
+	}
+	
+	public boolean primeNumbersVerification1(int n){
+		boolean aux = true;
+		for(int i = 2; i < n && aux; i++) {
+			if(n%i == 0) {
+				aux = false;
+			}
+		}
+		return aux;
+	}
+	
+	
+	
+	public boolean primeNumbersVerification2(int n){
+		boolean aux = true;
+		
+		if(n%2 == 0) {
+			aux = false;
+		}else {
+			for(int i = 3; i < n && aux; i+=2) {
+				if(n%i == 0) {
+					aux = false;
+				}
+			}
+		}
+		
+		return aux;
+	}
+
 }
