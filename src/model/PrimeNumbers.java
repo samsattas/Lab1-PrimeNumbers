8dec0d8113bbdf7d9535ad666c58bf362b85b8cf
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-02-05 17:03:54 -0500

    Alpha v1.7

--- src/model/PrimeNumbers.java
+++ src/model/PrimeNumbers.java
@@ -1,4 +1,6 @@
 package model;
+import java.lang.Math; 
+
 
 public class PrimeNumbers {
 
@@ -8,11 +10,16 @@ public class PrimeNumbers {
 	
 	public boolean primeNumbersVerification1(int n){
 		boolean aux = true;
-		for(int i = 2; i < n && aux; i++) {
-			if(n%i == 0) {
-				aux = false;
+		if(n==1) {
+			aux = false;
+		}else {
+			for(int i = 2; i < n && aux; i++) {
+				if(n%i == 0) {
+					aux = false;
+				}
 			}
 		}
+		
 		return aux;
 	}
 	
@@ -38,11 +45,39 @@ public class PrimeNumbers {
 	
 	public boolean primeNumbersVerification3(int n){
 		boolean aux = true;
-		for(int i = n - 1; i > 1 && aux; i--) {
-			if(n%i == 0) {
-				aux = false;
+		if(n==1) {
+			aux = false;
+		}else {
+			for(int i = n - 1; i > 1 && aux; i--) {
+				if(n%i == 0) {
+					aux = false;
+				}
 			}
 		}
+		return aux;
+	}
+	
+	
+	
+	public boolean Validation1Prime(int numberToValidate) {
+		int counter = 0;
+		boolean ok = false;
+		for(int i = 2; i <= numberToValidate; i++) {
+			if(numberToValidate % i == 0) {
+				counter++;
+			}
+		}
+		if(counter == 1) {
+			ok = true;
+		}
+		return ok;
+	}
+	
+	
+	
+	public int generateMatrix(int n) {
+		int aux = (int) Math.sqrt(n) + 1;
+		
 		return aux;
 	}
 
