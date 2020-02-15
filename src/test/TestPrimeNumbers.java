060562eefe558ea23bc811c09cfa615a502d3a24
JuanesSabogal <juan.ossa@hotmail.com>
2020-02-15 12:46:23 -0500

    Beta v1.7

--- src/test/TestPrimeNumbers.java
+++ src/test/TestPrimeNumbers.java
@@ -54,7 +54,7 @@ class TestPrimeNumbers {
 	public ArrayList<Boolean> setUpSceneIsPrimeValidation1Prime() {
 		ArrayList<Boolean> arrayWithBoolean = new ArrayList<>();
 		PrimeNumbers prime = new PrimeNumbers();
-		for(int i = 1; i<=7; i+=2) {
+		for(int i = 1; i<=7; i++) {
 			if(prime.Validation1Prime(i) == true) {
 				boolean toadd = prime.Validation1Prime(i);
 				arrayWithBoolean.add(toadd);
@@ -66,7 +66,7 @@ class TestPrimeNumbers {
 	public ArrayList<Boolean> setUpSceneIsPrimeValidation3Prime() {
 		ArrayList<Boolean> arrayWithBoolean = new ArrayList<>();
 		PrimeNumbers prime = new PrimeNumbers();
-		for(int i = 1; i<=7; i+=2) {
+		for(int i = 1; i<=7; i++) {
 			if(prime.primeNumbersVerification2(i) == true) {
 				boolean toadd = prime.primeNumbersVerification2(i);
 				arrayWithBoolean.add(toadd);
@@ -77,7 +77,7 @@ class TestPrimeNumbers {
 	public ArrayList<Boolean> setUpSceneIsPrimeValidation4Prime() {
 		ArrayList<Boolean> arrayWithBoolean = new ArrayList<>();
 		PrimeNumbers prime = new PrimeNumbers();
-		for(int i = 1; i<=7; i+=2) {
+		for(int i = 1; i<=7; i++) {
 			if(prime.primeNumbersVerification3(i) == true) {
 				boolean toadd = prime.primeNumbersVerification3(i);
 				arrayWithBoolean.add(toadd);
@@ -124,18 +124,18 @@ class TestPrimeNumbers {
 	@Test
 	void testIsPrimeWithArrayList() {
 		String info = setUpSceneIsPrimeValidation1Prime().toString();
-		assertEquals("[true, true, true]", info);
+		assertEquals("[true, true, true, true]", info);
 	}
 	
 	@Test
 	void testIsPrimeWithArrayListNbersVeri2() {
 		String info = setUpSceneIsPrimeValidation3Prime().toString();
-		assertEquals("[true, true, true, true]", info);
+		assertEquals("[true, true, true, true, true]", info);
 	}
 	@Test
 	void testIsPrimeWithArrayListNbersVeri3() {
 		String info = setUpSceneIsPrimeValidation4Prime().toString();
-		assertEquals("[true, true, true, true]", info);
+		assertEquals("[true, true, true, true, true]", info);
 	}
 	@Test
 	void testCovertToInt() {
